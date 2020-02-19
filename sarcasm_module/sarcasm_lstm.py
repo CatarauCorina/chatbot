bba5958d365c3f5a723e413d2abd01f0e4fda9a6
Corina Catarau <corinacatarau@corinas-mbp.home>
2020-02-19 01:43:18 +0000

    Multi head attention sarcasm

--- sarcasm_module/sarcasm_lstm.py
+++ sarcasm_module/sarcasm_lstm.py
@@ -1,9 +1,12 @@
 import torch.nn as nn
+from attention_lstm import AttentionMultiHead
 
 
 class LSTMSarcasm(nn.Module):
 
-    def __init__(self, vocabulary_size, embedding_dim, hidden_size, output_size, n_layers=2, dropout=0.1):
+    def __init__(self,
+                 vocabulary_size,
+                 embedding_dim, hidden_size, output_size, n_layers=2, dropout=0.1):
         super(LSTMSarcasm, self).__init__()
         self.embedding = nn.Embedding(
             num_embeddings=vocabulary_size,
@@ -15,10 +18,24 @@ class LSTMSarcasm(nn.Module):
             bidirectional=True,
             batch_first=True
         )
+        self.multi_head_attention = AttentionMultiHead(
+            input_size=embedding_dim,
+            hidden_size=embedding_dim,
+            nr_heads=8
+        )
+        self.fc1 = nn.Linear(embedding_dim, 100)
+        self.fc2 = nn.Linear(100, output_size)
+        self.dropout = nn.Dropout(dropout)
+        self.log_sigmoid = nn.LogSigmoid()
 
         return
 
     def forward(self, input_vector):
         embedding = self.embedding(input_vector)
         lstm_result = self.bi_lstm(embedding)
-        return
+        multi_head_attention = self.multi_head_attention(lstm_result)
+        out = self.fc1(multi_head_attention)
+        out = self.fc2(out)
+        out = self.dropout(out)
+        out_sigmoid = self.log_sigmoid(out)
+        return out_sigmoid
