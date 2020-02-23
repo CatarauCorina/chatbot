f7a876e5f4db5bf330ae56590e26eaf9fe1c3420
Corina Catarau <corinacatarau@Corinas-MacBook-Pro.local>
2020-02-23 02:46:23 +0200

    Small changes

--- sarcasm_module/sarcasm_lstm.py
+++ sarcasm_module/sarcasm_lstm.py
@@ -1,41 +1,95 @@
+import numpy as np
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from attention_lstm import AttentionMultiHead
+
+USE_CUDA = torch.cuda.is_available()
+device = torch.device("cuda" if USE_CUDA else "cpu")
 
 
 class LSTMSarcasm(nn.Module):
 
     def __init__(self,
                  vocabulary_size,
-                 embedding_dim, hidden_size, output_size, n_layers=2, dropout=0.1):
+                 embedding_dim,
+                 output_size,
+                 n_layers=2,
+                 hidden_dim_lstm=64,
+                 max_sentence_size=32,
+                 dropout=0.1):
         super(LSTMSarcasm, self).__init__()
         self.embedding = nn.Embedding(
             num_embeddings=vocabulary_size,
             embedding_dim=embedding_dim
         )
-        self.bi_lstm = nn.LSTM(
-            input_size=embedding_dim,
-            num_layers=2,
-            bidirectional=True,
-            batch_first=True
-        )
+        self.fw_lstm_layers = []
+        self.bw_lstm_layers = []
+        input_dim_lstm = embedding_dim
+        self.nr_layers = n_layers
+        for layer in range(n_layers):
+            lstm_lay_fw = nn.LSTM(
+                input_size=input_dim_lstm,
+                num_layers=1,
+                hidden_size=hidden_dim_lstm,
+                batch_first=True
+             ).to(device)
+            lstm_lay_bw = nn.LSTM(
+                input_size=input_dim_lstm,
+                num_layers=1,
+                hidden_size=hidden_dim_lstm,
+                batch_first=True
+            ).to(device)
+            input_dim_lstm = hidden_dim_lstm
+            self.fw_lstm_layers.append(lstm_lay_fw)
+            self.bw_lstm_layers.append(lstm_lay_bw)
+
+        lstm_output_concat_size = 2*hidden_dim_lstm + 2*hidden_dim_lstm + embedding_dim
         self.multi_head_attention = AttentionMultiHead(
-            input_size=embedding_dim,
-            hidden_size=embedding_dim,
+            input_size=lstm_output_concat_size,
+            hidden_size=lstm_output_concat_size,
             nr_heads=8
-        )
-        self.fc1 = nn.Linear(embedding_dim, 100)
-        self.fc2 = nn.Linear(100, output_size)
+        ).to(device)
+        self.linear = nn.Linear(lstm_output_concat_size*2, hidden_dim_lstm)
+        self.relu = nn.ReLU()
         self.dropout = nn.Dropout(dropout)
-        self.log_sigmoid = nn.LogSigmoid()
+        self.out = nn.Linear(hidden_dim_lstm, 1)
+        self.sigmoid = nn.Sigmoid()
 
         return
 
+    def reverse_tensor(self, tensor_to_reverse):
+        tensor_to_reverse = tensor_to_reverse.cpu().clone().detach()
+        to_reve_numpy = np.flip(tensor_to_reverse.numpy(), 2).copy()  # Reverse of copy of numpy array of given tensor
+        reversed_tensor = torch.from_numpy(to_reve_numpy)
+        reversed_tensor = torch.tensor(reversed_tensor, device=device).requires_grad_(True)
+        return reversed_tensor
+
     def forward(self, input_vector):
         embedding = self.embedding(input_vector)
-        lstm_result = self.bi_lstm(embedding)
-        multi_head_attention = self.multi_head_attention(lstm_result)
-        out = self.fc1(multi_head_attention)
-        out = self.fc2(out)
-        out = self.dropout(out)
-        out_sigmoid = self.log_sigmoid(out)
-        return out_sigmoid
+
+        #output, (hidden, cell) = self.bi_lstm(embedding)
+        lstm_input = embedding
+        reversed_input = self.reverse_tensor(lstm_input)
+        hidden_states = []
+        for idx, layer in enumerate(self.fw_lstm_layers):
+            fw = self.fw_lstm_layers[idx]
+            bw = self.bw_lstm_layers[idx]
+            output_fw, (hidden_fw, cell_fw) = fw(lstm_input)
+            output_bw, (hidden_bw, cell_bw) = bw(reversed_input)
+            concat_fw_bw = torch.cat([output_fw, output_bw], dim=2)
+            hidden_states.append(concat_fw_bw)
+            lstm_input = output_fw
+            reversed_input = output_bw
+
+        hidden_states.append(embedding)
+        cat_fw_bw_em = torch.cat(hidden_states, dim=2)
+
+        multi_head_attention = self.multi_head_attention(cat_fw_bw_em)
+        avg_pool = torch.mean(multi_head_attention, 1)
+        max_pool, _ = torch.max(multi_head_attention, 1)
+        conc = torch.cat((avg_pool, max_pool), 1)
+        conc = self.relu(self.linear(conc))
+        conc = self.dropout(conc)
+        out = self.out(conc)
+        return out
--- sarcasm_module/sarcasm_reddit_dataset.py
+++ sarcasm_module/sarcasm_reddit_dataset.py
@@ -7,71 +7,74 @@ import os, sys, re
 #
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from vocabulary import Vocabulary
 
 
 class RedditSarcasmDataset(data.Dataset):
-    START_TOKEN = "<S>"
-    END_TOKEN = "</S>"
 
     def __init__(self, **kwargs):
         self.stage = kwargs.get('stage', 'train')
         self.data_path = kwargs.get('path', '')
-        self.max_seq_length = kwargs.get('max_seq', 10)
+        self.max_seq_length = kwargs.get('max_seq', 30)
         self.stop_words = stopwords.words('english')
         self.freq_words = kwargs.get('frequent_word', [])
         # actual vocabulary
-        self.voc = kwargs.get('voc_index', None)
         self.file_path = os.path.join(self.data_path, f'{self.stage}-balanced-sarcasm.csv')
         self.redit_csv = pd.read_csv(self.file_path)
-        self.reddits = self.redit_csv['comment']
-        self.labels = self.redit_csv['label']
+        res_not_na = self.redit_csv[self.redit_csv['comment'].notna()]
+        res_lt_100 = self.redit_csv[self.redit_csv['comment'].str.len() < 100]
+
+        self.reddits = res_lt_100['comment']
+
+        self.voc = Vocabulary(f'{self.stage}-balanced-sarcasm.csv')
+
+        self.labels = res_lt_100['label']
+        self.create_reddit_vocabulary()
         # vocabulary keys
-        self.voc_keys = kwargs.get('voc_keys', [])
-        self.start_token = self.START_TOKEN
-        self.end_token = self.END_TOKEN
+        self.voc_keys = self.voc.word2index.keys()
+        self.start_token = self.voc.SOS_token
+        self.end_token = self.voc.EOS_token
         self.idx = None
         self.filename = None
         return
 
     def __len__(self):
-        return len(self.redit_csv['comment'][0])
+        return len(self.reddits)
 
     def tokenize_redit(self, s):
         s = word_tokenize(re.sub( '\W+', ' ', s.lower()))
         s = [w for w in s if w not in self.stop_words if w not in self.freq_words]
-        ind = [self.voc[w] if w in self.voc_keys else self.voc['<UNK>'] for w in s]
+        ind = [self.voc.word2index[w] for w in s if w in self.voc_keys]
         # pad or truncate the review
         if len(ind) >= self.max_seq_length:
             ind = ind[:self.max_seq_length]
         else:
             ind.extend([0]*(self.max_seq_length - len(ind)))
         # returns the list of indices
-        ind.insert(0, self.voc[self.start_token])
-        ind.append(self.voc[self.end_token])
+        ind.insert(0, self.start_token)
+        ind.append(self.end_token)
         return ind
 
-    def load_redit(self):
+    def create_reddit_vocabulary(self):
         for reddit in self.reddits:
-            seq = torch.tensor(self.tokenize_redit(reddit), dtype=torch.int)
-        return seq
+            self.voc.add_sentence(reddit)
+        return
+
+    def load_redit(self, idx):
+        tokenized_reddit = torch.tensor(self.tokenize_redit(self.reddits.iloc[idx]), dtype=torch.long)
+        return tokenized_reddit
 
     def __getitem__(self, idx):
         self.idx = idx
-        # get data - tensor of integers
         self.filename = self.file_path
         if self.stage == 'test':
             self.ret_filename()
-        seq = self.load_redit()
-        # get label-split the file name on dot and subscript, take the second value
-        split_review_name = re.split('[_.]', self.file_path)
-        if int(split_review_name[-2]) > 6:
-            lab = torch.tensor([1], dtype=torch.float)
-        else:
-            lab = torch.tensor([0], dtype=torch.float)
-        return seq, lab
+        seq = self.load_redit(idx)
+        return seq, self.labels.iloc[idx]
 
     def ret_filename(self):
         return self.filename
+
 
 def main():
     data_args = {'path': '../../data_general/sarcasm/'}
@@ -79,7 +82,8 @@ def main():
     pars = {'shuffle': False, 'batch_size': 1}
     dataset_stage = data.DataLoader(scr_data, **pars)
     for el in dataset_stage:
-        print(el.shape)
+        x, y = el
+        print(x.shape)
     return
 
 if __name__ == '__main__':
